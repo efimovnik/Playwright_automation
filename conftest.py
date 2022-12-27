f9143138c1f6ae06846c6b5a21371c139f36eaee
Nik Efimov <efimov.nikolai.ig@gmail.com>
2022-12-27 16:30:52 -0500

    reuse authentication state with storage file

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import os
 import time
 import pytest
-
+from playwright.sync_api import expect
 
 PASSWORD = os.environ['PASSWORD']
 
@@ -40,15 +40,18 @@ def context_creation(playwright):
     # page.get_by_label("Password").fill(utils.secret_config.PASSWORD)
     page.get_by_label("Password").fill(PASSWORD)
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+    # page.wait_for_load_state('networkidle')
+    time.sleep(2)
+    context.storage_state(path='state.json')
     yield context
 
 
 @pytest.fixture()
-def login_set_up(context_creation):
-    context = context_creation
+def login_set_up(context_creation, browser):
+    context = browser.new_context(storage_state='state.json')
     page = context.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.set_default_timeout(3000)
-
+    expect(page.get_by_text("Log In")).not_to_be_visible()
     yield page
     page.close()
