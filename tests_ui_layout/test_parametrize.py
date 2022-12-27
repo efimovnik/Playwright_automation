727dc88d427b4e42b5f0da0225cecb3467b82e72
Nik Efimov <efimov.nikolai.ig@gmail.com>
2022-12-27 14:22:33 -0500

    added parametrized login test

---
+++ tests_ui_layout/test_parametrize.py
@@ -0,0 +1,30 @@
+import time
+import pytest
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+@pytest.mark.parametrize("email, password", [("train.supersonic@gmail.com", "Playwrighttest123"),
+                                             ("train.supersonic", "Playwrighttest123"),
+                                             ("train.supersonic@gmail.com", "")])
+@pytest.mark.integration
+def test_login_parametrize(page, email, password) -> None:
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    page.set_default_timeout(3000)
+    login_issue = True
+    while login_issue:
+        if not page.get_by_test_id("signUp.switchToSignUp").is_visible():
+            page.get_by_text("Log In").click()
+        else:
+            login_issue = False
+        time.sleep(0.1)
+    page.get_by_test_id("signUp.switchToSignUp").click()
+    page.get_by_test_id("siteMembersDialogLayout").get_by_test_id("buttonElement").click()
+    page.get_by_test_id("emailAuth").get_by_label("Email").fill(email)
+    page.get_by_label("Password").fill(password)
+    page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+    page.get_by_role("button", name="train.supersonic account menu").click()
+    page.get_by_role("link", name="My Account").click()
+
+    # Assert - Then
+    expect(page.locator("//div[text()='Login Email']/following-sibling::div[1]")).to_have_text(
+        email)
